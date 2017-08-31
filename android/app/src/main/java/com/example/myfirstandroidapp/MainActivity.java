018a173942985db1b5c76eb173ab6f98da3a5062
GingerBear <bbbear444@gmail.com>
2017-08-31 16:18:04 -0400

    make react native work

--- android/app/src/main/java/com/example/myfirstandroidapp/MainActivity.java
+++ android/app/src/main/java/com/example/myfirstandroidapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myfirstandroidapp;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,12 +12,34 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MERSSAGE = "com.example.myfirstandroidapp.MESSAGE";
+    private static final int OVERLAY_PERMISSION_REQ_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+            }
+        }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    // SYSTEM_ALERT_WINDOW permission not granted...
+                }
+            }
+        }
+    }
+
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
@@ -23,6 +48,11 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MERSSAGE, message);
+        startActivity(intent);
+    }
+
+    public void openInbox(View view) {
+        Intent intent = new Intent(this, ReactNativeActivity.class);
         startActivity(intent);
     }
 }
